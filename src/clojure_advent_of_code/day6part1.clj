9dafeb22d7d5aa48d331b73ee05aa9222dd46608
Jack <jack@evans.gb.net>
2016-08-09 21:58:51 +0100

    properly parses command data

--- src/clojure_advent_of_code/day6part1.clj
+++ src/clojure_advent_of_code/day6part1.clj
@@ -6,9 +6,22 @@
 
 (def file-input (read-in-lines "resources/day6.txt"))
 
+(defn process-command [command]
+  (rest (re-find #"^.*(on|off|toggle)\s(\d*),(\d*)\sthrough\s(\d*),(\d*)$" command)))
+
+(defn read-instructions [board command]
+  (if (empty? command)
+    (println "done")
+    (do
+      (let [[cmd x1 x2 y1 y2] (process-command (first command))]
+        (println cmd x1 x2 y1 y2))
+      (read-instructions board (rest command)))))
+
+
+(def file-input (read-in-lines "resources/day6.txt"))
+
 (defn main []
-  (let [board (for [x (range 10) y (range 10)] (vector x y))]
-    (println board)
-    (println (type board))))
+  (let [board (zipmap (for [x (range 5) y (range 5)] (vector x y)) (repeat -1))]
+    (read-instructions board file-input)))
 
 (main)
