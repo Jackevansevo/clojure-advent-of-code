13296e51dbdf5e5cc97e41e2e6d79fcb53e9654b
Jack Evans <jack@evans.gb.net>
2016-08-29 09:43:25 +0100

    nearly finished day6

--- src/clojure_advent_of_code/day6part1.clj
+++ src/clojure_advent_of_code/day6part1.clj
@@ -6,13 +6,19 @@
 
 (def file-input (read-in-lines "resources/day6.txt"))
 
-(defn abs [n] (max n (- n)))
+(def grid-size 1000)
+
+(def grid (for [x (range grid-size) y (range grid-size)] [x y]))
+
+(defn on [n] 1)
+(defn off [n] -1)
+(defn toggle [n] (if (< n 0) 1 -1))
 
 (defn convert-inputs [thing]
   (case thing
-    "on" 1
-    "off" -1
-    "toggle" abs
+    "on" on
+    "off" off
+    "toggle" toggle
     (read-string thing)))
 
 (def command-regex #"^.*(on|off|toggle)\s(\d*),(\d*)\sthrough\s(\d*),(\d*)$")
@@ -20,21 +26,20 @@
 (defn process-command [command]
   (map convert-inputs (rest (re-find command-regex command))))
 
-(defn read-instructions [board command]
-  (if (empty? command)
-    (println "done")
+(defn map-values [m keys f & args]
+  (reduce #(apply update-in %1 [%2] f args) m keys))
+
+(defn read-instructions [board commands]
+  (if (empty? commands)
+    (println (count (filter #{1} (vals board))))
     (do
-      (let [[cmd x1 x2 y1 y2] (process-command (first command))]
-        (println x1 x2 y1 y2)
-      (read-instructions board (rest command))))))
+      (println "starting command:" (count commands))
+      (let [[cmd x1 x2 y1 y2] (process-command (first commands))]
+        (let [xStep (+ (* x1 grid-size) x2) yStep (inc (+ (* y1 grid-size) y2))]
+          (let [pointsBetween (into [] (drop xStep (take yStep grid)))]
+            (read-instructions (map-values board pointsBetween cmd) (rest commands))))))))
 
-(def file-input (read-in-lines "resources/day6.txt"))
-
-(defn main []
-  (let [board (into (sorted-map) (for [x (range 10) y (range 10)] [[x y] -1]))]
+(defn -main [& args]
+  (println "Starting day6")
+  (let [board (into (hash-map) (for [x (range grid-size) y (range grid-size)] [[x y] -1]))]
     (read-instructions board file-input)))
-
-
-(for [x (range 1 3) y (range 1 6)] [x y])
-
-(main)
